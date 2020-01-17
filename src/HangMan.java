fd235871cff2c21923d1845222cda0a17f6175b8
SimonVestman <simonw202@gmail.com>
2020-01-17 11:15:15 +0100

    A start to the game choosing system.

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HangMan {
     public static  void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to play against a other player write player or a computer write computer");
+        String Gamemode = Scanner.nextLine();
+        if(Gamemode = "computer"){
+            System.out.println("Hej då");
 
+            else if(Gamemode = "player"){
+                System.out.println("hej");
+            }
+        }
     }
 }
