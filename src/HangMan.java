b7c9e1a6dcb706ac8f1341a662808f94f528e8d8
SimonVestman <simonw202@gmail.com>
2020-01-23 11:15:29 +0100

    The start of the singelplayer gamemode

--- src/HangMan.java
+++ src/HangMan.java
@@ -8,14 +8,29 @@ import java.util.Scanner;
 
 public class HangMan {
     public static  void main (String [] args){
+
+    //Chose singel or multiplayer
         Scanner sc = new Scanner(System.in);
         System.out.println("Do you want to play against a other player or a computer");
         String Gamemode = sc.nextLine();
-
         if(Gamemode.equalsIgnoreCase("player")) {
-            System.out.println("You are now playing against an ohter player");
+            //Singelplayer begins
+            System.out.println("You are now playing against an other player");
+            boolean win = false;
+            String temp = "";
+            String hold = "";
+            String guessed = "";
+
+            System.out.println("Player 1 can now enter the secret word!");
+                String w = sc.nextLine();
+                do {
+                    System.out.println("Player 2 can now guess!");
+                    String guess = sc.nextLine();
+                } while (){
+            }
 
         } else if (Gamemode.equalsIgnoreCase("computer")){
+            //Multiplayer begins
                 System.out.println("You are now playing against a computer");
         }
     }
